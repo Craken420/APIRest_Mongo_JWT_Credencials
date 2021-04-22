e491d0f75e890925b8a54e441fbe770b19c08a31
Luis Angel Peña <lapena@mavi.mx>
2021-04-22 14:50:38 -0500

    fix:: Validate the token provide in the access middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,18 +2,22 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 
 const auth = async(req, res, next) => {
-   const token = req.header('Authorization').replace('Bearer ', '')
-   const data = jwt.verify(token, process.env.JWT_KEY)
-   try {
-      const user = await User.findOne({ _id: data._id, 'tokens.token': token })
-      if (!user) {
-         throw new Error()
+   const token = req.header('Authorization')
+   if (token) {
+      const token = token.replace('Bearer ', '')
+      const data = jwt.verify(token, process.env.JWT_KEY)
+      try {
+         const user = await User.findOne({ _id: data._id, 'tokens.token': token })
+         if (!user) {
+            throw new Error()
+         }
+         req.user = user
+         req.token = token
+         next()
+      } catch (error) {
+         res.status(401).send({ error: 'Not authorized to access this resource' })
       }
-      req.user = user
-      req.token = token
-      next()
-   } catch (error) {
-      res.status(401).send({ error: 'Not authorized to access this resource' })
-   }
+   } else
+      res.status(401).send({ error: 'Token not provided'})
 }
 module.exports = auth
