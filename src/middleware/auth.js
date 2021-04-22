dd1d51b7f0f56555db2cd7ed3a4c2ec2b8592139
Luis Angel Peña <lapena@mavi.mx>
2021-04-22 14:56:54 -0500

    feat:: Validate expiration of the access token

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,19 +5,32 @@ const auth = async(req, res, next) => {
    const token = req.header('Authorization')
    if (token) {
       const token = token.replace('Bearer ', '')
-      const data = jwt.verify(token, process.env.JWT_KEY)
+      jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+         if (err)
+             next(err);  //Error, trying to access unauthorized page!
+         else {
+             if (decoded.exp <= Date.now()) {
+                 var err = new Error("Access token has expired");
+                 next(err);  //Error, trying to access unauthorized page!
+             }
+             req.decoded = decoded;
+         }
+      });
       try {
-         const user = await User.findOne({ _id: data._id, 'tokens.token': token })
+         const user = await User.findOne({ _id: req.decoded._id, 'tokens.token': token })
          if (!user) {
-            throw new Error()
+            let err = new Error("User token not found");
+            next(err);
          }
          req.user = user
          req.token = token
          next()
       } catch (error) {
-         res.status(401).send({ error: 'Not authorized to access this resource' })
+         next(error);
       }
-   } else
-      res.status(401).send({ error: 'Token not provided'})
+   } else {
+      let err = new Error("Token not provided");
+      next(err);
+   }
 }
 module.exports = auth
